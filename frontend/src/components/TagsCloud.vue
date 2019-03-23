157ce09fd1d55dff8e519f6469251ebbbc5ec13a
Marcin Krawczyk <marckraw@icloud.com>
2019-03-23 16:35:02 +0100

    [add] navigation options for adding and filtering, clean up boilerplate subpages

---
+++ frontend/src/components/TagsCloud.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="tags-cloud">
+    <h1>This is TagsCloud</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "tags-cloud"
+};
+</script>
+
+<style>
+</style>
