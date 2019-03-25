4000c2bc17d98759692b1ee69c3d555ff9195f88
Marcin Krawczyk <marcin.krawczyk@ef.com>
2019-03-25 10:43:05 +0100

    Add one hardcoded link to lib in javascript

---
+++ frontend/src/data/hardcodedData.js
@@ -0,0 +1,9 @@
+export const libs = [
+  {
+    id: 0,
+    name: "Swiper",
+    short_description: "Most modern mobile touch slider",
+    description: "Most modern mobile touch slider",
+    url: "https://idangero.us/swiper/"
+  }
+];
