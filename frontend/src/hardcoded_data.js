3a5c16ac8dbc3967b0accf88e96c8d994f034b4b
Marcin Krawczyk <marckraw@icloud.com>
2019-03-27 12:56:22 +0100

    Hardcoded_data in other place

---
+++ frontend/src/hardcoded_data.js
@@ -0,0 +1,22 @@
+export const reources = [
+  {
+    id: 0,
+    tags: ["css"],
+    title: "Center positioning",
+    short_description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae velit ut leo commodo dictum. Vestibulum egestas leo ac sapien ultricies elementum. Aliquam iaculis vestibulum dolor, a vehicula ex consequat ac.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae velit ut leo commodo dictum. Vestibulum egestas leo ac sapien ultricies elementum. Aliquam iaculis vestibulum dolor, a vehicula ex consequat ac. Aliquam lectus sem, semper sit amet sagittis non, sodales nec lacus. Vivamus tempus facilisis libero a viverra. Sed neque velit, ullamcorper ac vehicula sit amet, tincidunt non nulla. Suspendisse blandit nisl vitae aliquet viverra. Duis venenatis, mauris sollicitudin molestie vulputate, dui nibh egestas diam, non consequat justo dui sit amet massa. Quisque diam augue, lobortis quis magna a, interdum rhoncus metus. Pellentesque luctus, augue in tincidunt semper, elit sem vehicula risus, in hendrerit magna enim id nibh. Donec dictum volutpat aliquet. Suspendisse blandit massa vitae urna hendrerit gravida. Nunc sagittis enim nisl, nec pretium nulla condimentum ac. Vivamus posuere sed turpis eu dictum.",
+    markdown: ""
+  },
+  {
+    id: 1,
+    tags: ["javascript"],
+    title: "Normal onClick",
+    short_description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae velit ut leo commodo dictum. Vestibulum egestas leo ac sapien ultricies elementum. Aliquam iaculis vestibulum dolor, a vehicula ex consequat ac.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae velit ut leo commodo dictum. Vestibulum egestas leo ac sapien ultricies elementum. Aliquam iaculis vestibulum dolor, a vehicula ex consequat ac. Aliquam lectus sem, semper sit amet sagittis non, sodales nec lacus. Vivamus tempus facilisis libero a viverra. Sed neque velit, ullamcorper ac vehicula sit amet, tincidunt non nulla. Suspendisse blandit nisl vitae aliquet viverra. Duis venenatis, mauris sollicitudin molestie vulputate, dui nibh egestas diam, non consequat justo dui sit amet massa. Quisque diam augue, lobortis quis magna a, interdum rhoncus metus. Pellentesque luctus, augue in tincidunt semper, elit sem vehicula risus, in hendrerit magna enim id nibh. Donec dictum volutpat aliquet. Suspendisse blandit massa vitae urna hendrerit gravida. Nunc sagittis enim nisl, nec pretium nulla condimentum ac. Vivamus posuere sed turpis eu dictum.",
+    markdown: ""
+  }
+];
