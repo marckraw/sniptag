6d5fdaeb0e8d6d9e18d4e78163d8a8f6d87530c7
Marcin Krawczyk <marckraw@icloud.com>
2019-03-27 12:55:51 +0100

    Some changes

---
+++ frontend/src/components/FilteredResults.vue
@@ -0,0 +1,18 @@
+<template>
+  <h2>Results</h2>
+</template>
+
+<script>
+export default {
+  name: "filtered-results",
+  props: {
+    data: {
+      type: Object,
+      required: false
+    }
+  }
+};
+</script>
+
+<style>
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <TagsCloud/>
+    <FilteredResults/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import TagsCloud from "@/components/TagsCloud.vue";
+import FilteredResults from "@/components/FilteredResults.vue";
 
 export default {
   name: "home",
   components: {
-    TagsCloud
+    TagsCloud,
+    FilteredResults
   }
 };
 </script>
